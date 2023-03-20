3f36c388f4592fe53d5622b5591ecbfa520eecc6
Valentine Munialo <valentynjoy@gmail.com>
2023-03-20 12:18:00 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printf "with proper grammar, but the outcome is a
+ *  piece of art,
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("\n%c", c";
+
+			return (0);
+}
