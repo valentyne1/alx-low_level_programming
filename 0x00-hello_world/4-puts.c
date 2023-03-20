85645efdf650715176eeef48e4736f797fd7986a
Valentine Munialo <valentynjoy@gmail.com>
2023-03-20 12:06:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "Progamming is like building a multilingual 
+ * main - prints "Progamming is like building a multilingual
  * puzzle, followed by a new line.
  * Return: Always 0.
  */
