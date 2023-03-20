cbdf045bbe5f3cf46b291b0f1b1da2bce7375017
Valentine Munialo <valentynjoy@gmail.com>
2023-03-20 15:27:26 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char j;
+
+for (j = 'z'; j >= 'a'; j--)
+putchar(j);
+
+putchar('\n');
+return (0);
+}
