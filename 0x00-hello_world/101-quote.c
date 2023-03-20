6e9385c40fecd615c7b6fc3d5a452e04c1241853
Valentine Munialo <valentynjoy@gmail.com>
2023-03-20 13:47:00 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
 * main - A c file to display a string to the standard err.
-* Return - 0 signifying program success
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-   return (1);
+return (1);
 }
