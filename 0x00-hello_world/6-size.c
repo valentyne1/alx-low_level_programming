05400f1de608cc0e2269988728e79a5201b45847
Valentine Munialo <valentynjoy@gmail.com>
2023-03-20 13:06:06 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - A c file to display the sizes of built in data types in c
+* Return - 0 signifying program success
+*/
+int main(void)
+{
+
+
+   printf("Size of a char: %lu byte(s)\n", sizeof(char));
+   printf("Size of an int: %lu byte(s)\n", sizeof(int));
+   printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+   printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+   printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+   return (0);
+}
