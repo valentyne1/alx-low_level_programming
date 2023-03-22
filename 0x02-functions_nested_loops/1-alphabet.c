14c7464350818761dd7a1adcdac860cdede574be
Valentine Munialo <valentynjoy@gmail.com>
2023-03-22 12:19:02 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char v;
+
+	for (v = 'a'; v <= 'z'; v++)
+	{
+	_putchar(v);
+	}
+	_putchar('\n');
+}
