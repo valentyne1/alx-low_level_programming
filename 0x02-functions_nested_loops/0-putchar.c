9eb645ffc50885892301027f5e6afe8445e2dcd5
Valentine Munialo <valentynjoy@gmail.com>
2023-03-22 10:31:17 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
+ * main - prints _putchar
+ *
+ * Description long description
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-printf("_putchar\n");
+	int position;
+	char *string;
 
-return (0);
+	string = "_putchar";
+
+	for (position = 0 ; string[position] != '\0' ; position++)
+	_putchar(string[position]);
+	_putchar('\n');
+	return (0);
 }
-
