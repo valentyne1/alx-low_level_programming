c9208460f6998486532ebeed2068401a651f29af
Valentine Munialo <valentynjoy@gmail.com>
2023-03-23 06:02:26 -0700

    Alphabets in uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ *
+ * @c: Character
+ *
+ * Return: Always 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c >= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
