f0a76aca7a510f3a769be1bb2c0d0f7c3960ab26
Valentine Munialo <valentynjoy@gmail.com>
2023-03-23 07:10:47 -0700

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * @a: first integer
+ * @b: srcond integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
