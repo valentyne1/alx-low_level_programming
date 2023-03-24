4afdac923859ab2cf9b462769f2e8ed804b6772c
Valentine Munialo <valentynjoy@gmail.com>
2023-03-24 16:55:40 -0700

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_triangle - function to print triangle
+ * @size: parameters
+ * Return: Always 0
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ *@n: integers
+ */
+
+void print_number(int n)
+{
+	_putchar(n);
+}
