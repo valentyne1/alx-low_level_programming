b9855015c69d93f3e973e00fb4ffad3bdba7c749
Valentine Munialo <valentynjoy@gmail.com>
2023-03-27 08:48:32 -0700

    swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - function to swap integers
+ * @a: first integer
+ * @b: second integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	*b = 98;
+	*a = 42;
+}
