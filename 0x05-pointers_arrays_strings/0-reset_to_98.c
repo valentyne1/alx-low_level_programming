e3a1afba40fb64ba87eef475c127e8795a08ffbe
Valentine Munialo <valentynjoy@gmail.com>
2023-03-27 08:30:07 -0700

    pointer reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Entry point
+ * @n: the integer
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+Pointers in C
