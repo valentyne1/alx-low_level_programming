05c55b80030185ed7fc0202ff4c34b4a15fe9b9b
Valentine Munialo <valentynjoy@gmail.com>
2023-03-28 13:30:03 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,4 +7,17 @@
  */
 void rev_string(char *s)
 {
-  char ch, *p, *q;
+  int len = 0;
+  int index = 0;
+  char tmp;
+
+  while(s[index++])
+	  len++;
+
+		  for (index = len - 1; index >= len/2; index --)
+		  {
+			  tmp = s[index];
+			  s[index] = s[len - index - 1];
+			  s[len - index -1] = tmp;
+		  }
+}
