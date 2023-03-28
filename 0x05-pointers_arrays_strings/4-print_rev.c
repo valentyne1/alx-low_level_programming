e34db8164dcafd4669f55fed569afbfa867b9f7b
Valentine Munialo <valentynjoy@gmail.com>
2023-03-28 13:18:21 -0700

    print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_rev - prints a string in reverse
  * @s: string
@@ -7,12 +7,14 @@
 
 void print_rev(char *s)
 {
-s = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-index($s);
-
-for (index
-	g = s;
-
-	
-	print_rev(g);
+	int g = 0;
+	while (s[g] != '\0')
+	{
+		g++;
+	}
+	for (g -= 1; g >= 0; g--)
+	{
+		_putchar(s[g]);
+	}
+	_putchar('\n');
 }
