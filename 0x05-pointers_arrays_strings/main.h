11b0975304c623a1c2fb5f866f8ec69b81fa453f
Valentine Munialo <valentynjoy@gmail.com>
2023-03-28 13:59:11 -0700

    prints every other character of a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 void reset_to_98(int *n);
 int _putchar(char c);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
