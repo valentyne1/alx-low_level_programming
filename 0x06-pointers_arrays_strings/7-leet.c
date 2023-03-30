66bc05750d4c64fd69c67ccf2d25a51ffc7511ac
Valentine Munialo <valentynjoy@gmail.com>
2023-03-30 13:17:31 -0700

    encoding a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
+#include "main.h"
+/**
+ * leet - main entry
+ * @str: string
+ * Return: returns the string
+ */
+char *leet(char *str)
+{
+int i;
+char c;
 
-char *leet(char *str) {
-    char *leet_equivs[] = {"4", "3", "0", "7", "1"};
-    char *letters[] = {"aA", "eE", "oO", "tT", "lL"};
-    int len = strlen(str);
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < len; j++) {
-            if (strchr(letters[i], str[j])) {  // if str[j] is in letters[i]
-                str[j] = leet_equivs[i][0];
-            }
-        }
-    }
-    return str;
+for (i = 0; str[i] != '\0'; i++)
+{
+c = str[i];
+if (c == 'a' || c == 'A')
+{
+str[i] = '4';
 }
-
+else if (c == 'e' || c == 'E')
+{
+str[i] = '3';
+}
+else if (c == 'o' || c == 'O')
+{
+str[i] = '0';
+}
+else if (c == 't' || c == 'T')
+{
+str[i] = '7';
+}
+else if (c == 'l' || c == 'L')
+{
+str[i] = '1';
+}
+}
+return (str);
+}
