aa8fd7f4791f011b278b5e1a584deee24fc1eead
Valentine Munialo <valentynjoy@gmail.com>
2023-04-03 12:28:46 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strchr - function that locates a character
+ * @s: the pointer
+ * @c the character
+ * Return: Always a pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int h;
+
+	for (h = 0; s[h] >= '\0'; h++)
+	{
+		if (s[h] == c)
+			return (s + h);
+	}
+	return (NULL);
+}
