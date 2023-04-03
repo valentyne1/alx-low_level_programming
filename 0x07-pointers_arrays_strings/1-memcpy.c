ac855c15b8cf46750e78b59987d09b931d1ba569
Valentine Munialo <valentynjoy@gmail.com>
2023-04-03 12:18:21 -0700

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_memcpy - function to copy memory address
+ *@dest: destination memory
+ *@src: source memory
+ *@n: number of bytes
+ *Return: Always dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+		dest[j] = src[j];
+	return (dest);
+}
